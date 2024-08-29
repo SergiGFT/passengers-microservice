c7ba218ee38fa21574410019c438d39934c2d89c
SergiGFT <sipm@gft.com>
2024-08-29 18:08:21 +0200

    first commit

---
+++ src/test/java/com/gft/workshop/passengers/PassengersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.workshop.passengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
